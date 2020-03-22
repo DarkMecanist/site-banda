4738d44f4ce55156fa56a29d9398c74eeddbb026
Joao Moutinho <joao.moutinho@gmail.com>
2020-03-22 00:58:21 +0000

    v3

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+function set_main_content() {
+    var content = "Sou o milhor";
+
+    document.getElementById("main-content").textContent = content;
+
+    console.log(document.getElementById("main-content").textContent)
+}
